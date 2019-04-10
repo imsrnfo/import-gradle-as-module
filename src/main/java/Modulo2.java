38679eb902acb5c7ce9c02956aabaf6ab7acf1ac
imsrnfo <isilva@isaltda.com.uy>
2019-04-10 12:38:18 -0300

    Init

---
+++ src/main/java/Modulo2.java
@@ -0,0 +1,9 @@
+public class Modulo2 {
+
+    public static void main(String ...  args){
+        System.out.println("modulo 2");
+        Libreria lib = new Libreria();
+        lib.metodo1();
+    }
+
+}
